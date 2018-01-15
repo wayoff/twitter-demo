9061004af9b206a3ddd39af43996323d21795947
Mark Jestoni Suner <mark.suner@outlook.com>
2018-01-15 11:49:57 +0100

    fix callback config

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ return [
 	'twitter' => [
 		'key' => '98dUj951440MVeZnU9Dz9T2Xu',
 		'secret' => 'tfnGYEh8SN2ZQVHAJj19DyFL0YyXW3FKkn9HviPDk9spRkhWGE',
-		'callback' => 'http://local.twitter.com/callback.php'
+		'callback' => 'http://twitter-demo-demo-twitter.7e14.starter-us-west-2.openshiftapps.com/callback.php'
 	]
 ];
